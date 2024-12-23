5136269ed32dda3d40c0275bad14b555ce3f7ee9
Omadbek080804 <odilovo575@gmail.com>
2024-12-23 14:49:48 +0500

    detail/canon

--- src/views/Detail/DetailCanon.jsx
+++ src/views/Detail/DetailCanon.jsx
@@ -1,12 +1,165 @@
 import React from 'react'
 import canon from '../../assets/images/canon.png'
+import Macbro from '../../assets/images/MacBro.png'
+import { IoEyeOutline } from "react-icons/io5";
+import { IoLocationSharp } from "react-icons/io5";
 
 function DetailCanon() {
   return (
     <div className='detail-box'>
-
 <h1>Похожие объявления</h1>
-      
+
+<div className="conteiner-canon">
+
+<div className='canon-card'>
+
+
+      <img src={canon} className='canon' alt="" />
+
+      <div className="macbro-canon">
+        <div className="macbro-canon-left">
+          <img src={Macbro} alt="" />
+          <h1>Macbro</h1>
+        </div>
+        <div className="macbro-canon-left">
+        <IoEyeOutline className='eye-canon'/>
+          <h1>124</h1>
+        </div>
+      </div>
+
+<h1 className='canon-text'>Фотоаппарат Canon EOS 2000D  Kit EF-S 18-55mm <br /> III IS Wi-Fi</h1>
+<div className="canon-price">
+  <h2>4 700 000 сум </h2>
+  <h1>3 900 000 сум</h1>
+</div>
+
+<div className="canon-location">
+  <div className="canon-location-card">
+<IoLocationSharp/>
+<h1>Ташкент</h1>
+
+
+  </div>
+  <div className="canon-location-card">
+<h1>14 но. – 21 дек.</h1>
+
+
+  </div>
+</div>
+</div>
+<div className='canon-card'>
+
+
+      <img src={canon} className='canon' alt="" />
+
+      <div className="macbro-canon">
+        <div className="macbro-canon-left">
+          <img src={Macbro} alt="" />
+          <h1>Macbro</h1>
+        </div>
+        <div className="macbro-canon-left">
+        <IoEyeOutline className='eye-canon'/>
+          <h1>124</h1>
+        </div>
+      </div>
+
+<h1 className='canon-text'>Фотоаппарат Canon EOS 2000D  Kit EF-S 18-55mm <br /> III IS Wi-Fi</h1>
+<div className="canon-price">
+  <h2>4 700 000 сум </h2>
+  <h1>3 900 000 сум</h1>
+</div>
+
+<div className="canon-location">
+  <div className="canon-location-card">
+<IoLocationSharp/>
+<h1>Ташкент</h1>
+
+
+  </div>
+  <div className="canon-location-card">
+<h1>14 но. – 21 дек.</h1>
+
+
+  </div>
+</div>
+</div>
+<div className='canon-card'>
+
+
+      <img src={canon} className='canon' alt="" />
+
+      <div className="macbro-canon">
+        <div className="macbro-canon-left">
+          <img src={Macbro} alt="" />
+          <h1>Macbro</h1>
+        </div>
+        <div className="macbro-canon-left">
+        <IoEyeOutline className='eye-canon'/>
+          <h1>124</h1>
+        </div>
+      </div>
+
+<h1 className='canon-text'>Фотоаппарат Canon EOS 2000D  Kit EF-S 18-55mm <br /> III IS Wi-Fi</h1>
+<div className="canon-price">
+  <h2>4 700 000 сум </h2>
+  <h1>3 900 000 сум</h1>
+</div>
+
+<div className="canon-location">
+  <div className="canon-location-card">
+<IoLocationSharp/>
+<h1>Ташкент</h1>
+
+
+  </div>
+  <div className="canon-location-card">
+<h1>14 но. – 21 дек.</h1>
+
+
+  </div>
+</div>
+</div>
+<div className='canon-card'>
+
+
+      <img src={canon} className='canon' alt="" />
+
+      <div className="macbro-canon">
+        <div className="macbro-canon-left">
+          <img src={Macbro} alt="" />
+          <h1>Macbro</h1>
+        </div>
+        <div className="macbro-canon-left">
+        <IoEyeOutline className='eye-canon'/>
+          <h1>124</h1>
+        </div>
+      </div>
+
+<h1 className='canon-text'>Фотоаппарат Canon EOS 2000D  Kit EF-S 18-55mm <br /> III IS Wi-Fi</h1>
+<div className="canon-price">
+  <h2>4 700 000 сум </h2>
+  <h1>3 900 000 сум</h1>
+</div>
+
+<div className="canon-location">
+  <div className="canon-location-card">
+<IoLocationSharp/>
+<h1>Ташкент</h1>
+
+
+  </div>
+  <div className="canon-location-card">
+<h1>14 но. – 21 дек.</h1>
+
+
+  </div>
+</div>
+</div>
+
+
+
+</div>
+
     </div>
   )
 }
